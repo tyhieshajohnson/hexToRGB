93a199ff5e9474356bbed0ff54b7fde23189a3be
tyhieshajohnson <johnsontyhiesha@gmail.com>
2023-11-27 13:01:37 +0200

    javascript

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 document.getElementById('convertBtn').addEventListener('click', function() {
     // Get the hex input value
-    var hexInput = document.getElementById('hexInput').value;
+    let hexInput = document.getElementById('hexInput').value;
 
     // Check if the input is a valid hex color
     if (!/^#[0-9A-F]{6}$/i.test(hexInput)) {
@@ -9,7 +9,7 @@ document.getElementById('convertBtn').addEventListener('click', function() {
     }
 
     // Convert hex to RGB
-    var rgb = hexToRgb(hexInput);
+    let rgb = hexToRgb(hexInput);
 
     // Display the result
     document.getElementById('redInput').value = rgb.r;
@@ -22,12 +22,12 @@ function hexToRgb(hex) {
     hex = hex.replace(/^#/, '');
 
     // Parse the hex value
-    var bigint = parseInt(hex, 16);
+    let bigint = parseInt(hex, 16);
 
     // Extract RGB components
-    var r = (bigint >> 16) & 255;
-    var g = (bigint >> 8) & 255;
-    var b = bigint & 255;
+    let r = (bigint >> 16) & 255;
+    let g = (bigint >> 8) & 255;
+    let b = bigint & 255;
 
     return { r, g, b };
 }
